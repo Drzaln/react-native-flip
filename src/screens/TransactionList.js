867572b4a69eee7f94be24cda3147a83cfc10241
Drzaln <doddyrizaln@gmail.com>
2021-08-21 13:16:12 +0700

    feat: add TransactionList UI

--- src/screens/TransactionList.js
+++ src/screens/TransactionList.js
@@ -1,61 +1,180 @@
 import React from 'react';
 import {
-  Alert,
-  Button,
-  Modal,
+  FlatList,
   Pressable,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import {TransactionContext} from '../provider/TransactionProvider';
+import {
+  changeToProperCase,
+  changeToUpperCase,
+  currencyFormatter,
+  formatDateToDMY,
+  formatRupiah,
+  formatter,
+} from '../utils';
+
+const Item = ({item}) => {
+  const {
+    id,
+    amount,
+    status,
+    sender_bank,
+    beneficiary_bank,
+    beneficiary_name,
+    created_at,
+  } = item;
+  return (
+    <Pressable
+      onPress={() => {}}
+      style={{
+        backgroundColor: 'white',
+        borderRadius: 4,
+        flexDirection: 'row',
+        alignItems: 'center',
+        overflow: 'hidden',
+      }}>
+      <View style={{flex: 0.05}}>
+        <View
+          style={{
+            width: 5,
+            height: 55,
+            backgroundColor: status === 'SUCCESS' ? '#58b485' : '#fb6544',
+          }}
+        />
+      </View>
+      <View style={{flex: 0.65, paddingVertical: 16}}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{fontWeight: 'bold'}}>
+            {sender_bank.length === 3
+              ? changeToUpperCase(sender_bank)
+              : changeToProperCase(sender_bank)}
+          </Text>
+          <Entypo
+            name="arrow-right"
+            size={20}
+            color="black"
+            style={{marginHorizontal: 4}}
+          />
+          <Text style={{fontWeight: 'bold'}}>
+            {beneficiary_bank.length <= 4
+              ? changeToUpperCase(beneficiary_bank)
+              : changeToProperCase(beneficiary_bank)}
+          </Text>
+        </View>
+        <Text style={{fontWeight: '500'}}>{beneficiary_name}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={{fontWeight: '500'}}>{currencyFormatter(amount)}</Text>
+          <Entypo
+            name="dot-single"
+            size={20}
+            color="black"
+          />
+          <Text style={{fontWeight: '500'}}>{formatDateToDMY(created_at)}</Text>
+        </View>
+      </View>
+      <View style={{flex: 0.3, alignItems: 'flex-end', paddingRight: 12}}>
+        <View
+          style={{
+            paddingHorizontal: 8,
+            paddingVertical: 3,
+            borderRadius: 8,
+            backgroundColor: status === 'SUCCESS' ? '#58b485' : 'white',
+            borderWidth: 2,
+            borderColor: status === 'SUCCESS' ? 'white' : '#fb6544',
+          }}>
+          <Text
+            style={{
+              fontWeight: '600',
+              color: status === 'SUCCESS' ? 'white' : 'black',
+            }}>
+            {status === 'SUCCESS' ? 'Berhasil' : 'Pengecekan'}
+          </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
 
 const TransactionList = ({navigation}) => {
-  const [modalVisible, setModalVisible] = React.useState(false);
+  const {transactions} = React.useContext(TransactionContext);
+  const renderItem = ({item}) => <Item item={item} />;
   return (
-    <View>
+    <>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
-      <Text>Transaction List</Text>
-      <Button
-        onPress={() => navigation.navigate('TransactionDetail')}
-        title="Navigate"
-        color="#841584"
-        accessibilityLabel="Navigate"
+      <SearchBar />
+      <FlatList
+        data={transactions}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <View style={{height: 12}} />}
+        contentContainerStyle={{paddingHorizontal: 20, paddingBottom: 20}}
+        showsVerticalScrollIndicator={false}
       />
-      <Button
-        onPress={() => setModalVisible(true)}
-        title="Modal"
-        color="green"
-        accessibilityLabel="Modal"
-      />
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <Pressable
-          style={{
-            ...StyleSheet.absoluteFill,
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onPress={() => setModalVisible(!modalVisible)}>
-          <View>
-            <Text>Hello World!</Text>
-            <Pressable onPress={() => setModalVisible(!modalVisible)}>
-              <Text>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </Pressable>
-      </Modal>
-    </View>
+    </>
   );
 };
 
 export default TransactionList;
 
 const styles = StyleSheet.create({});
+
+const SearchBar = () => {
+  const [text, onChangeText] = React.useState('');
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        borderRadius: 4,
+        marginBottom: 16,
+        marginTop: 20,
+        marginHorizontal: 20,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          flex: 2,
+        }}>
+        <EvilIcon
+          name="search"
+          size={24}
+          color="#cbcbcb"
+          style={{marginRight: 4}}
+        />
+        <TextInput
+          style={{height: 40, width: '90%'}}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Cari nama, bank, atau nominal"
+        />
+      </View>
+      <Pressable
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          flex: 0.9,
+          justifyContent: 'flex-end',
+        }}>
+        <Text style={{color: '#f47c61', fontWeight: 'bold'}}>URUTKAN</Text>
+        <Entypo
+          name="chevron-small-down"
+          size={24}
+          color="#f47c61"
+          style={{marginLeft: 4}}
+        />
+      </Pressable>
+    </View>
+  );
+};
